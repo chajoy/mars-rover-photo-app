203c73664e43d5703f9e66edc3b0d186dbfd4c9a
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-01-28 11:57:58 +0000

    Setup serverless function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.scss";
+
+const getData = async () => {
+  try {
+    const response = await fetch("/.netlify/functions/api/api.js?name=Test");
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const data = await response.text();
+    console.log(data);
+  } catch (error) {
+    console.error(`${error.message}`);
+  }
+};
+
+getData();
